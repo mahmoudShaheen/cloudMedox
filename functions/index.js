5b173a5733987939f3fd8c24abfe49853e29cb0b
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-03-16 13:01:24 +0200

    adding promise handler

--- functions/index.js
+++ functions/index.js
@@ -14,34 +14,45 @@ exports.sendDatabaseNotification = functions.database.ref('/messages/{messageID}
   console.log('messageID:  ', messageID);
 
   //get message content
-  const token = admin.database().ref(`/messages/${messageID}/to`).once('value').toString();
-  const level = admin.database().ref(`/messages/${messageID}/level`).once('value').toString();
-  const message = admin.database().ref(`/messages/${messageID}/message`).once('value').toString();
-  const title = admin.database().ref(`/messages/${messageID}/title`).once('value').toString();
+  const tokenPromise = admin.database().ref(`/messages/${messageID}/to`).once('value');
+  const levelPromise = admin.database().ref(`/messages/${messageID}/level`).once('value');
+  const messagePromise = admin.database().ref(`/messages/${messageID}/message`).once('value');
+  const titlePromise = admin.database().ref(`/messages/${messageID}/title`).once('value');
 
-  // message details.
-  const payload = {
-    data: {
-      title: `${title}`,
-      body: `${message}`,
-      level: `${level}`
-    }
-  };
+  return Promise.all([tokenPromise, levelPromise, messagePromise, titlePromise]).then(results => {
+    const token = results[0];
+    const level = results[1];
+    const message = results[2];
+    const title = results[3];
 
-  // Set the message as high priority and have it expire after 24 hours.
-  const options = {
-    priority: "high",
-    timeToLive: 60 * 60 * 24
-  };
+    console.log(token);
+    console.log(level);
+    console.log(message);
+    console.log(title);
+    // message details.
+    const payload = {
+      data: {
+        title: `${title}`,
+        body: `${message}`,
+        level: `${level}`
+      }
+    };
+    console.log(payload)
+    // Set the message as high priority and have it expire after 24 hours.
+    const options = {
+      priority: "high",
+      timeToLive: 60 * 60 * 24
+    };
 
 
-  // Send notification to device.
-  admin.messaging().sendToDevice(token, payload, options).then(response => {
-    //delete message from database
-    admin.database().ref(`/messages/${messageID}`).set({});
-    console.log("Successfully sent message: ", response);
-  })
-  .catch(function(error) {
-    console.log("Error sending message: ", error);
+    // Send notification to device.
+    admin.messaging().sendToDevice(token, payload, options).then(response => {
+      //delete message from database
+      admin.database().ref(`/messages/${messageID}`).set({});
+      console.log("Successfully sent message: ", response);
+    })
+    .catch(function(error) {
+      console.log("Error sending message: ", error);
+    });
   });
 });
