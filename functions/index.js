5d949917376188493d5ca61ea419edb4f9223423
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-03-16 11:10:35 +0200

    :pray:  Suppose to run :pray:

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
 //initializeApp
 'use strict';
-var functions = require('firebase-functions');
+const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+//main lambda function
 exports.sendDatabaseNotification = functions.database.ref('/messages/{messageID}}').onWrite(event => {
   const messageID = event.params.messageID;
   // If message deleted function return
@@ -11,3 +12,36 @@ exports.sendDatabaseNotification = functions.database.ref('/messages/{messageID}
     return console.log('Deleted messageID:  ', messageID);
   }
   console.log('messageID:  ', messageID);
+
+  //get message content
+  const token = admin.database().ref(`/messages/${messageID}/to`).once('value');
+  const level = admin.database().ref(`/messages/${messageID}/level`).once('value');
+  const message = admin.database().ref(`/messages/${messageID}/message`).once('value');
+  const title = admin.database().ref(`/messages/${messageID}/title`).once('value');
+
+  // message details.
+  const payload = {
+    data: {
+      title: `${title}`,
+      body: `${message}`,
+      level: `${level}`
+    }
+  };
+
+  // Set the message as high priority and have it expire after 24 hours.
+  const options = {
+    priority: "high",
+    timeToLive: 60 * 60 * 24
+  };
+
+
+  // Send notification to device.
+  admin.messaging().sendToDevice(token, payload, options).then(response => {
+    //delete message from database
+    admin.database().ref(`/messages/${messageID}`).set({});
+    console.log("Successfully sent message: ", response);
+  })
+  .catch(function(error) {
+    console.log("Error sending message: ", error);
+  });
+});
